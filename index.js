c9952655e1d00f9c2713fc3193499c25e99a586f
fumihiko_hidaka <5.p8\nABYk>
2018-09-08 13:21:06 +0900

    add privacy

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index')
+});
+
+app.get('/policy', (req, res) => {
+  res.render('privacy')
 });
 
 app.listen(process.env.PORT || 8080);
